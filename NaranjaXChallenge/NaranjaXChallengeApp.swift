da5ed48a5ef1a433fb5fec5edef82aaa6cc12c44
Manuel Moyano <manuelm_98@hotmail.com>
2022-06-06 08:06:34 -0300

    Initial Commit

---
+++ NaranjaXChallenge/NaranjaXChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaranjaXChallengeApp.swift
+//  NaranjaXChallenge
+//
+//  Created by Manuel Moyano on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct NaranjaXChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
