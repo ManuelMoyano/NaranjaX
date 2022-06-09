db9392bb289eaa88682690ea13a9122127bde636
Manuel Moyano <manuelm_98@hotmail.com>
2022-06-08 21:58:14 -0300

    Primer archivo

--- NaranjaXChallenge/ContentView.swift
+++ NaranjaXChallenge/ContentView.swift
@@ -8,9 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    let keyplataform = "eb0274aa-bad4-4eeb-b8fe-05ac8f0c0b15"
+    @State private var response = EndpointResponse(response: Response.example)
+    let viewTitle = "Noticias"
+    @State private var page = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                List (response.response.results, id: \.id) {item in
+                        Text(item.webTitle)
+                }
+            }
+            .task {
+                 await loadData()
+             }
+            .navigationTitle(viewTitle)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItem(placement: .bottomBar) {
+                    HStack {
+                        Button {
+                            if page > 1 {
+                                page -= 1
+                                Task {
+                                await loadData()
+                                }
+                            }
+                        } label: {
+                            Image(systemName: "minus")
+                        }
+                        Text ("\(page)")
+                        Button {
+                            if page < response.response.pages {
+                                page += 1
+                                Task {
+                                await loadData()
+                            }
+                            }
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func loadData() async {
+    //    Creating the URL we want to read.
+    //    Fetching the data for that URL.
+    //    Decoding the result of that data into a struct.
+        guard let url = URL(string: "https://content.guardianapis.com/search?page=\(page)&q=format=json&from-date=2022-01-01&order-by=relevance&api-key=\(keyplataform)") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+    //      El guion bajo en data descarta los metadatos y se queda solo con los datos de la URL
+            let (data, _) = try await URLSession.shared.data(from: url)
+            print("Se ingreso a la funcion")
+            if let decodedResponse = try? JSONDecoder().decode(EndpointResponse.self, from: data) {
+                response = decodedResponse
+            }
+            
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
@@ -19,3 +82,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
---
+++ NaranjaXChallenge/Response.swift
@@ -0,0 +1,84 @@
+//
+//  Response.swift
+//  NaranjaXChallenge
+//
+//  Created by Manuel Moyano on 06/06/2022.
+//
+
+import Foundation
+
+
+struct EndpointResponse: Codable {
+    var response: Response
+    
+    static let example = EndpointResponse(response: Response.example)
+}
+
+struct Response: Codable {
+    var status: String
+    var userTier: String
+    var total: Int
+    var startIndex:Int
+    var pageSize:Int
+    var currentPage:Int
+    var pages:Int
+    var orderBy:String
+    var results = [Result]()
+    
+//    init(status:String, userTier: String, total: Int, startIndex: Int, pageSize:Int, currentPage: Int, pages: Int, orderBy: String) {
+//        self.status = status
+//        self.userTier = userTier
+//        self.total = total
+//        self.startIndex = startIndex
+//        self.pageSize = pageSize
+//        self.currentPage = currentPage
+//        self.pages = pages
+//        self.orderBy = orderBy
+//        self.results = [Result]()
+
+//    }
+    
+    static let example = Response( status:"ok",userTier:"user tier",total:10,startIndex:1,pageSize:10,currentPage:1,pages:29,orderBy:"relevance")
+    
+    
+}
+
+
+
+
+struct Result: Codable {
+
+    var id: String
+    var sectionId: String
+    var sectionName: String
+    var webPublicationDate: String
+    var webTitle: String
+    var webUrl: String
+    var apiUrl: String
+    
+    
+//    static let example = Result(status: "Status", total: 10, currentPage: "One", id: "id", sectionName: "sectionName", webUrl: "webUrl", apiUrl: "apiUrl")
+    
+}
+
+//1- Listado de noticias
+//Cada ítem deberá tener:
+//● Título
+//● Fecha de publicación
+//● Imagen
+//2- Detalle del artículo:
+//● Imagen
+//● Fecha de publicación
+//● Título
+//● Cabecera
+//● Cuerpo de la noticia
+//(puede ser el texto plano o el formato enriquecido usando el Html)
+
+
+//https://content.guardianapis.com/search?q=space&format=json&from-date=2021-
+//01-01&show-tags=contributor&show-fields=starRating,headline,thumbnail,short
+//-url&order-by=relevance&api-key=<API-KEY>
+
+//https://content.guardianapis.com/science/2021/jun/01/international-space-st
+//ation-robotic-arm-space-junk?show-tags=contributor&show-fields=all&api-key=
+//<API-KEY>
